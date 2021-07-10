1e8a153c707ef82603e80b96af337c88cf458f0f
Khanin Choudhury <khanin.choudhury@scientificgames.com>
2021-07-10 23:03:32 +0530

    Initial project commit. Added basic functionality

---
+++ src/Json.Net.Curl/Json.Net.Curl.Test/CurlUnitTest.cs
@@ -0,0 +1,119 @@
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace Json.Net.CurlTest
+{
+    public class CurlTests
+    {
+        const string GET_JSON_URL = "https://mocki.io/v1/35587ebf-59a2-41f7-9b93-aedec64af78a";
+        const string GET_JSON_ARRAY_URL = "https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8";
+
+        [SetUp]
+        public void Setup()
+        {
+          
+        }
+
+        #region get 
+        [Test]
+        public void GetJson()
+        {
+            var json = Json.Net.Curl.Get(@"data\JObjectUnitTest1.json");
+            Assert.IsNotNull(json);
+        }
+        [Test]
+        public void GetJsonAsync()
+        {
+            var json = Json.Net.Curl.GetAsync(@"data\JObjectUnitTest1.json").GetAwaiter().GetResult();
+            Assert.IsNotNull(json);
+        }
+        [Test]
+        public void GetJsonFromRemote()
+        {
+            var json = Json.Net.Curl.Get(GET_JSON_URL);
+            Assert.IsNotNull(json);
+        }
+        [Test]
+        public void GetJsonFromRemoteAsync()
+        {
+            var json = Json.Net.Curl.GetAsync(GET_JSON_URL).GetAwaiter().GetResult();
+            Assert.IsNotNull(json);
+        }
+        [Test]
+        public void GetJsonArray()
+        {
+            var json = Json.Net.Curl.GetJArray(@"data\JArrayUnitTest1.json");
+            Assert.IsNotNull(json);
+        }
+        [Test]
+        public void GetJsonArrayFromRemote()
+        {
+            var json = Json.Net.Curl.GetAsync(GET_JSON_ARRAY_URL);
+            Assert.IsNotNull(json);
+        }
+        [Test]
+        public void GetJsonArrayFromRemoteAsync()
+        {
+            var json = Json.Net.Curl.GetJArrayAsync(GET_JSON_ARRAY_URL).GetAwaiter().GetResult();
+            Assert.IsNotNull(json);
+        }
+        #endregion
+
+        #region save 
+        [Test]
+        public void Save()
+        {
+            Json.Net.Curl.Save(@"data\JObjectSave1.json", new Newtonsoft.Json.Linq.JObject() { 
+            ["id"] = "1",
+            ["user"] = "Khanin",
+            ["pass"] = "AAA@@@!!!!"
+            });
+            Assert.Pass();
+        }
+        [Test]
+        public void SaveAsync()
+        {
+            Json.Net.Curl.SaveAsync(@"data\JObjectSaveAsync1.json", new Newtonsoft.Json.Linq.JObject()
+            {
+                ["id"] = "1",
+                ["user"] = "Khanin",
+                ["pass"] = "AAA@@@!!!!"
+            }).GetAwaiter().GetResult();
+            Assert.Pass();
+        }
+        [Test]
+        public void SaveJArray()
+        {
+            Json.Net.Curl.SaveJArray(@"data\JObjectSaveJArray1.json", new JArray(){ new JObject()
+            {
+                ["id"] = "1",
+                ["user"] = "Khanin",
+                ["pass"] = "AAA@@@!!!!"
+            } });
+            Assert.Pass();
+        }
+        [Test]
+        public void SaveJArrayAsync()
+        {
+            Json.Net.Curl.SaveJArrayAsync(@"data\JObjectSaveJArrayAsync1.json", new JArray(){ new JObject()
+            {
+                ["id"] = "1",
+                ["user"] = "Khanin",
+                ["pass"] = "AAA@@@!!!!"
+            } }).GetAwaiter().GetResult();
+            Assert.Pass();
+        }
+        [Test]
+        public void SaveRemote()
+        {
+            Json.Net.Curl.Save(@"https://jsonnetcurl.free.beeceptor.com/add", new Newtonsoft.Json.Linq.JObject()
+            {
+                ["id"] = "1",
+                ["user"] = "Khanin",
+                ["pass"] = "AAA@@@!!!!"
+            });
+            Assert.Pass();
+        }
+        #endregion
+    }
+}
---
+++ src/Json.Net.Curl/Json.Net.Curl/Curl.cs
@@ -0,0 +1,192 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json.Net
+{
+    public static class Curl
+    {
+        private const long TIMEOUT_MS = (1000 * 60) * 3;
+        public static JObject Get(string location, Dictionary<string, string> metadata = null, long timeoutMS = TIMEOUT_MS)
+        {
+            return GetAsync(location, metadata, timeoutMS).GetAwaiter().GetResult();
+        }
+        public static async Task<T> GetAsync<T>(string location, Dictionary<string, string> metadata = null, long timeoutMS = TIMEOUT_MS)
+        {
+            if (IsLocalFile(location))
+            {
+                if (File.Exists(location))
+                {
+                    return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(await File.ReadAllTextAsync(location));
+                }
+                else
+                {
+                    throw new FileNotFoundException($"Path : {location}");
+                }
+            }
+            else
+            {
+                using (var handler = new HttpClientHandler())
+                {
+                    handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+                    handler.ServerCertificateCustomValidationCallback =
+                        (httpRequestMessage, cert, cetChain, policyErrors) =>
+                        {
+                            return true;
+                        };
+                    using (HttpClient client = new HttpClient(handler, true))
+                    {
+                        AddHeaders(client, metadata);
+                        client.Timeout = TimeSpan.FromMilliseconds(timeoutMS);
+                        var result = await client.GetAsync(location);
+                        if (result.IsSuccessStatusCode)
+                        {
+                            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(await result.Content.ReadAsStringAsync());
+                        }
+                        else
+                        {
+                            throw new HttpRequestException($"HttpStatus: {result.StatusCode}");
+                        }
+                    }
+                }
+            }
+        }
+     
+        public static async Task<JObject> GetAsync(string location, Dictionary<string, string> metadata = null, long timeoutMS = TIMEOUT_MS)
+        {
+            return await GetAsync<JObject>(location, metadata,timeoutMS);
+        }
+        private static void AddHeaders(HttpClient httpClient, Dictionary<string, string> headers)
+        {
+            if (headers != null)
+            {
+                foreach (var header in headers)
+                {
+                    if (header.Key.ToLower() == "authorization")
+                    {
+                        var authValues = header.Value.Split(' ');
+                        if (authValues.Length == 2)
+                        {
+                            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(authValues[0], authValues[1]);
+                        }
+                    }
+                    else if (header.Key.ToLower() == "content-type") { }
+                    else
+                    {
+                        httpClient.DefaultRequestHeaders.Add(header.Key, header.Value);
+                    }
+                }
+            }
+        }
+        public static JArray GetJArray(string location, Dictionary<string, string> metadata = null, long timeoutMS = TIMEOUT_MS)
+        {
+            return GetJArrayAsync(location, metadata,timeoutMS).GetAwaiter().GetResult();
+        }
+        public static async Task<JArray> GetJArrayAsync(string location, Dictionary<string, string> metadata = null, long timeoutMS = TIMEOUT_MS)
+        {
+            return await GetAsync<JArray>(location, metadata, timeoutMS);
+        }
+
+        public static void Save(string location, JObject data, Dictionary<string, string> metadata = null, HttpMethod method = null, long timeoutMS = TIMEOUT_MS)
+        {
+            Save<object>(location, data.ToString(), metadata, method, timeoutMS).GetAwaiter().GetResult();
+        }
+        public static async Task SaveAsync(string location, JObject data, Dictionary<string, string> metadata = null, HttpMethod method = null, long timeoutMS = TIMEOUT_MS)
+        {
+            await Save<object>(location, data.ToString(), metadata, method, timeoutMS);
+        }
+        public static void SaveJArray(string location, JArray data, Dictionary<string, string> metadata = null, HttpMethod method = null, long timeoutMS = TIMEOUT_MS)
+        {
+            Save<object>(location, data.ToString(), metadata, method, timeoutMS).GetAwaiter().GetResult();
+        }
+        public static async Task SaveJArrayAsync(string location, JArray data, Dictionary<string, string> metadata = null, HttpMethod method = null, long timeoutMS = TIMEOUT_MS)
+        {
+            await Save<object>(location, data.ToString(), metadata, method, timeoutMS);
+        }
+        public static async Task<T> Save<T>(string location, string data, Dictionary<string, string> metadata = null, HttpMethod method = null, long timeoutMS = TIMEOUT_MS)
+        {
+            if (IsLocalFile(location))
+            {
+
+
+                await File.WriteAllTextAsync(location, data);
+                return default(T);
+
+            }
+            else
+            {
+                using (var handler = new HttpClientHandler())
+                {
+                    handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+                    handler.ServerCertificateCustomValidationCallback =
+                        (httpRequestMessage, cert, cetChain, policyErrors) =>
+                        {
+                            return true;
+                        };
+                    using (HttpClient client = new HttpClient(handler, true))
+                    {
+                        if(method == null)
+                        {
+                            method = HttpMethod.Post;
+                        }
+
+                        AddHeaders(client, metadata);
+                        client.Timeout = TimeSpan.FromMilliseconds(timeoutMS);
+
+                        var bytearraycontent = new ByteArrayContent(
+                      Encoding.UTF8.GetBytes(data));
+                        bytearraycontent.Headers.ContentType = GetContentType(metadata);
+
+                        var request = new HttpRequestMessage(method, location)
+                        {
+                            Content = bytearraycontent
+                        };
+                        var result = await client.SendAsync(request);
+                        if (!result.IsSuccessStatusCode)
+                        {
+                            throw new HttpRequestException($"HttpStatus: {result.StatusCode}");
+                        }
+                        else
+                        {
+                            if (typeof(T) != typeof(object))
+                            {
+                                return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(await request.Content.ReadAsStringAsync());
+                            }
+                            else
+                            {
+                                return default(T);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private static  MediaTypeHeaderValue GetContentType(Dictionary<string, string> headers)
+        {
+            var contetType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            if (headers != null)
+            {
+                var ctype = headers.Where(f => f.Key.ToLower() == "content-type").
+                    Select(f =>
+                    {
+                        contetType = new System.Net.Http.Headers.MediaTypeHeaderValue(f.Value);
+                        return true;
+                    });
+            }
+            return contetType;
+        }
+
+        private static bool IsLocalFile(string location)
+        {
+            location = location.Trim().ToLower();
+            return !(location.IndexOf("http") == 0 || location.IndexOf("https") == 0);
+        }
+    }
+}
